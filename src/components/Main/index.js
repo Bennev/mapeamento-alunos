a91f117d50f355af58f31b021c889a28951b2799
Matheus Benevides <mb.militao@gmail.com>
2022-06-09 10:18:33 -0300

    inicia testes com a api

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,23 @@
-import React, {useEffect, useState} from 'react'
 import "./style.css"
+
+import React, { useEffect, useState } from 'react'
+
 import Map from '../Map'
 import Sidebar from '../Sidebar'
+import api from '../../api'
 
 export default function Main() {
-    const [campus, setCampus] = useState()
+  const [campus, setCampus] = useState()
 
-    return (
-        <main>
-            <Map  campus={campus} setCampus={setCampus} />
-            <Sidebar campus={campus} setCampus={setCampus} />
-        </main>
-    )
+  useEffect(() => {
+    // lembra que eu disse que se eu colocar um useEffect com o array de dependencias vazio ele só vai entneder que é pra rodar no começo quando componente for renderizar? show, pois bora fazer um teste buscando dados da api, aqui eu vou buscar dados dos cursos, show dms
+    api.get('/cota')
+  }, [])
+
+  return (
+    <main>
+      <Map campus={campus} setCampus={setCampus} />
+      <Sidebar campus={campus} setCampus={setCampus} />
+    </main>
+  )
 }
